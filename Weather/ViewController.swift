394f024d87bab0335a9560f55ebbc0eefa2947f1
GarifullinII <ildaridrisovich@gmail.com>
2025-05-12 17:04:08 +0400

    initial commit
    add files

---
+++ Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Ildar Garifullin on 12.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
